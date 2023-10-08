74155df416f357480c15037e1c6919db67d2774f
DinhHuuPhuong <muoi1772002@gmail.com>
2023-10-09 01:36:19 +0700

    bai tap lap trinh di dong tuan 4

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import _2a from './data/_2a';
+import _2b from './data/_2b';
+import _2c from './data/_2c';
+import Tiki_Ok from './data/Tiki_Ok';
+
+export default function App() {
+  return (
+    // _2a()
+    // _2b()
+    // _2c()
+    Tiki_Ok()
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
